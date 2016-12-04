9dafa3049257a963213b4a59b350dc720cacea87
@hejiaqiya <1182111947@qq.com>
2016-12-04 19:36:08 +0800

    update zuoye

--- app/src/scripts/demos/zuoye.js
+++ app/src/scripts/demos/zuoye.js
@@ -1,38 +1,40 @@
 angular.module("myApp",[])
 .controller("zController",["$scope",function($scope){
-	var messages=[
+	var userInfo=[
 		{
-			FirstName:"FANG",
-			LastName:"vane",
-			Salary:"￥12,333.50",
-			Birthday:"2007-07-11"
+			firstname:"FANG",
+			lastname:"vane",
+			salary:"1233350",
+			birthday:new Date("2007-07-11")
 		},
 		{
-			FirstName:"SARA",
-			LastName:"rose",
-			Salary:"￥232,334.23",
-			Birthday:"1997-02-03"
+			firstname:"SARA",
+			lastname:"rose",
+			salary:"23233423",
+			birthday:new Date("1997-02-03")
 		},
 		{
-			FirstName:"AAM",
-			LastName:"hot",
-			Salary:"￥66,880.50",
-			Birthday:"1986-03-06"
+			firstname:"AAM",
+			lastname:"hot",
+			salary:"6688050",
+			birthday:new Date("1986-03-06")
 		},
 		{
-			FirstName:"MARK",
-			LastName:"bear",
-			Salary:"￥68,000.00",
-			Birthday:"2001-11-16"
+			firstname:"MARK",
+			lastname:"bear",
+			salary:"6800000",
+			birthday:new Date("2001-11-16")
 		}
 	]
-	$scope.messages=messages;
+	$scope.userInfo=userInfo;
 	$scope.firstName="";
 	$scope.lastName="";
+	$scope.sortColumn="firstname";  //默认排序字段
+	$scope.reverseSort=false;   //默认的排序方式
 
 	$scope.searchFirst=function(obj){
 		if($scope.firstName!=""){
-			if(obj.FirstName.toUpperCase().indexOf($scope.firstName.toUpperCase())!=-1){
+			if(obj.firstname.toUpperCase().indexOf($scope.firstName.toUpperCase())!=-1){
 				return true;
 			}else{
 				return false;
@@ -43,7 +45,7 @@ angular.module("myApp",[])
 	}
 	$scope.searchLast=function(dat){
 		if($scope.lastName!=""){
-			if(dat.LastName.toLowerCase().indexOf($scope.lastName.toLowerCase())!=-1){
+			if(dat.lastname.toLowerCase().indexOf($scope.lastName.toLowerCase())!=-1){
 				return true;
 			}else{
 				return false;
@@ -53,6 +55,17 @@ angular.module("myApp",[])
 		}
 	}
 
-	$scope.col = 'FirstName';
-    $scope.desc = 0;
+	//设置排序字段
+	$scope.setSort=function(column){
+		//如果传递过来的排序字段和当前字段一致，则是降序，否则升序
+		$scope.reverseSort=$scope.sortColumn==column?!$scope.reverseSort:false;
+		$scope.sortColumn=column;
+	}
+	
+	//设置类
+	$scope.getClass=function(column){
+		if($scope.sortColumn==column){
+			return $scope.reverseSort?"down":"up";
+		}
+	}
 }])
